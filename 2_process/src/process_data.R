19a06405ec5923371c47a93ced647e27bccba969
Erica Rotman <ericarotman@Ericas-MacBook-Pro.local>
2021-07-16 12:49:26 -0400

    pipeline-ify file naming to be more generic in saving model diagnostics

--- 2_process/src/process_data.R
+++ 2_process/src/process_data.R
@@ -20,7 +20,7 @@ write_data <-  function(eval_data, file_out) {
 }
 
 
-save_diagnostics <- function(eval_data, project_output_dir, file_diag){
+save_diagnostics <- function(eval_data, file_out){
   
   # Save the model diagnostics
   render_data <- list(pgdl_980mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
@@ -38,6 +38,6 @@ save_diagnostics <- function(eval_data, project_output_dir, file_diag){
   ({{dl_500mean}} and {{pb_500mean}}°C, respectively) or more, but worse than PB when training was reduced to 100 profiles ({{dl_100mean}} and {{pb_100mean}}°C respectively) or fewer.
   The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively). '
   
-  whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file = file.path(project_output_dir, 'model_diagnostic_text.txt'))
+  whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file=file_out)
   
 }
