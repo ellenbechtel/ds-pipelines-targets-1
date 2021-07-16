fa7c374a0cb288bf64bb1fcc77432974aafa3cb8
Erica Rotman <ericarotman@Ericas-MacBook-Pro.local>
2021-07-16 12:50:31 -0400

    remove file.path()

--- 3_visualize/src/plot_data.R
+++ 3_visualize/src/plot_data.R
@@ -1,7 +1,7 @@
 plot_data <- function(plot_out, eval_data){
   
   # Create a plot
-  png(file = file.path(plot_out), width = 8, height = 10, res = 200, units = 'in')
+  png(file = plot_out, width = 8, height = 10, res = 200, units = 'in')
   par(omi = c(0,0,0.05,0.05), mai = c(1,1,0,0), las = 1, mgp = c(2,.5,0), cex = 1.5)
   
   plot(NA, NA, xlim = c(2, 1000), ylim = c(4.7, 0.75),
